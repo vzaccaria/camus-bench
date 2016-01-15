77dd208f636f4ad0f94bd34d4615ec5027b4ebd0
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-01-15 15:54:19 +0100

    fix: cast number of requests to int

--- examples/script.sh
+++ examples/script.sh
@@ -1,2 +1,3 @@
-#DEBUG=* ../index.js distribution -t exponential -u "http://www.google.com" -d ./payload.json -g 100 --lambda 1.0 --get
-../index.js service-time -u "http://www.google.com" -d ./payload.json -g 100 --get -n 1
+DEBUG=* ../index.js service-time -u "http://www.google.com" -d ./payload.json -g 100 --get -n 3
+DEBUG=* ../index.js distribution -t exponential -u "http://www.google.com" -d ./payload.json -g 100 --get -n 3
+DEBUG=* ../index.js distribution -t fixed -u "http://www.google.com" -d ./payload.json -g 100 --get -n 3
